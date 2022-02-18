36a0e9d464be72705e1895652fb96e287c11156e
mstclair@millionconcepts.com <mstclair@millionconcepts.com>
2022-02-18 14:29:17 -0500

    clean up apxs table special case signatures

--- pdr/formats/msl_apxs.py
+++ pdr/formats/msl_apxs.py
@@ -1,6 +1,6 @@
 def table_loader(data, pointer):
     # we don't support these right now, or maybe ever
     if pointer == "ERROR_CONTROL_TABLE":
-        return True, data.tbd
+        return data.tbd
     # clarifying SCIENCE_HEADER_TABLE-- it is a TABLE, not a HEADER
-    return True, data.read_table
+    return data.read_table
