dffccbfad64c3a3235180029e283f957fbbdb87f
m-stclair <mstclair@millionconcepts.com>
2022-04-27 10:41:05 -0400

    use the correct classifier for beta

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://github.com/MillionConcepts/pdr",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Development Status :: 2 - Beta",
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
