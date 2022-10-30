9b03422a9baa21272e011b2eb7d750d5fc9567ae
Sierra V. Kaufman <sierra@millionconcepts.com>
2022-10-30 08:52:26 -0400

    bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pdr",
-    version="0.7.1",
+    version="0.7.2",
     author="Chase Million",
     author_email="chase@millionconcepts.com",
     description="Planetary Data Reader",
