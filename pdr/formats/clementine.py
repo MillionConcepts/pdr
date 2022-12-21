010dd91c615b8c869d987f88540188926d9e960f
Sierra-MC <sierra@millionconcepts.com>
2022-12-21 14:42:46 -0500

    Read the real and imaginary parts of the numbers separately.

--- pdr/formats/clementine.py
+++ pdr/formats/clementine.py
@@ -1,5 +1,5 @@
 import re
-
+import pandas as pd
 from pdr.pd_utils import insert_sample_types_into_df
 
 
@@ -15,7 +15,8 @@ def get_fn(data, object_name):
 
 def get_structure(pointer, data):
     fmtdef = data.read_table_structure(pointer)
-    fmtdef.ITEM_OFFSET = 16
-    fmtdef.ITEM_BYTES = 16
+    pd.concat([fmtdef, fmtdef])
+    fmtdef.ITEM_OFFSET = 8
+    fmtdef.ITEM_BYTES = 8
     fmtdef, dt = insert_sample_types_into_df(fmtdef, data)
     return True, fmtdef, dt
