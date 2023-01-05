768f3bde58fa80653db756c703a9f79f6759978c
Sierra-MC <sierra@millionconcepts.com>
2023-01-05 14:10:09 -0500

    Added BINARY CODED DECIMAL as a datatype because apparently sometimes it's an acronym and sometimes it's not.

--- pdr/datatypes.py
+++ pdr/datatypes.py
@@ -70,7 +70,8 @@ def sample_types(
         "CHARACTER": f"S{sample_bytes}",  # ASCII character string
         "TIME": f"S{sample_bytes}",
         "VOID": f"{void}{sample_bytes}",
-        "BCD": f"{void}{sample_bytes}"
+        "BCD": f"{void}{sample_bytes}",
+        "BINARY CODED DECIMAL": f"{void}{sample_bytes}"
     }[sample_type]
 
 
