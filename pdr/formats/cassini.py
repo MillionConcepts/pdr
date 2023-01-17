aa7048cee594b39523e6428624896f4afe81d764
Sierra-MC <sierra@millionconcepts.com>
2023-01-17 09:42:23 -0500

    Change cassini ppi table loader to depend on if it's full resolution or not

--- pdr/formats/cassini.py
+++ pdr/formats/cassini.py
@@ -3,10 +3,10 @@ def ppi_table_loader(data, pointer, data_set_id):
     def load_this_table(*_, **__):
         import pandas as pd
         structure = data.read_table_structure(pointer)
-        if data_set_id == "CO-S-MIMI-4-CHEMS-CALIB-V1.0" or "CO-S-MIMI-4-INCA-CALIB-V1.0":
-            skiprows = 7
-        if data_set_id == "CO-S-MIMI-4-LEMMS-CALIB-V1.0":
+        if "FULL" in data.file_mapping[pointer]:
             skiprows = 4
+        else:
+            skiprows = 7
         table = pd.read_csv(data.file_mapping[pointer], skiprows=skiprows, header=None, names=structure.NAME)
         return table
 
