6067e313c43affc53e4e12308ee8f608ceea0f98
michael <mstclair@millionconcepts.com>
2023-02-07 13:17:08 -0500

    remember to make iterable

--- pdr/formats/core.py
+++ pdr/formats/core.py
@@ -202,7 +202,7 @@ def check_special_case(pointer, data) -> tuple[bool, Optional[Callable]]:
         return True, formats.diviner.diviner_l4_table_loader(data, pointer)
     if re.match(r"CO-(CAL-ISS|S-ISSNA/ISSWA-2)", ids["DATA_SET_ID"]):
         if pointer in ("TELEMETRY_TABLE", "LINE_PREFIX_TABLE"):
-            return data.trivial
+            return True, data.trivial
     return False, None
 
 
