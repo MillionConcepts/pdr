f40d22738adbbc41795e9a152e49268980d548db
curtiss9 <98858647+curtiss9@users.noreply.github.com>
2023-03-13 18:34:21 -0400

    Reorder if statement for mgn special case to avoid error from mgn_gravity

--- pdr/formats/core.py
+++ pdr/formats/core.py
@@ -169,9 +169,9 @@ def check_special_case(pointer, data) -> tuple[bool, Optional[Callable]]:
         # unsigned integers not specified as such
         return True, formats.lroc.lroc_edr_image_loader(data, pointer)
     if (
-        ids["SPACECRAFT_NAME"] == "MAGELLAN"
-        and data.metaget_("NOTE").startswith("Geometry")
+        ids["SPACECRAFT_NAME"] == "MAGELLAN" 
         and pointer == "TABLE"
+        and data.metaget_("NOTE").startswith("Geometry")
     ):
         return True, formats.mgn.geom_table_loader(data, pointer)
     if (
