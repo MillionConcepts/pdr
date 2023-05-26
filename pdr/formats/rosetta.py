bd5c1fe185d26dc4bf3099dbd1f0d89937718f8d
Sierra-MC <sierra@millionconcepts.com>
2023-05-26 16:36:44 -0400

    remove uncessary imports

--- pdr/formats/rosetta.py
+++ pdr/formats/rosetta.py
@@ -1,7 +1,4 @@
-from typing import Callable
-
-
-def rosetta_table_loader(filename, fmtdef_dt) -> Callable:
+def rosetta_table_loader(filename, fmtdef_dt):
     import astropy.io.ascii
     table = astropy.io.ascii.read(filename).to_pandas()
     fmtdef, dt = fmtdef_dt
