625f081aeff5ae2b9eac1328edf18ed386722efb
m-stclair <mstclair@millionconcepts.com>
2023-09-29 00:52:19 -0400

    more tidying

--- pdr/tests/test_metadata.py
+++ pdr/tests/test_metadata.py
@@ -11,6 +11,3 @@ def test_metadata_1():
     )['ARTICULATION_DEVICE_ANGLE'][0]['units'] == 'rad'
     assert meta.metaget_fuzzy('KAT') == 'MEOW.CAT'
     assert meta.metaget_('MEOW_SEQUENCE_NUMBERS') == (1, 2, 3, 4, '5')
-
-
-test_metadata_1()
