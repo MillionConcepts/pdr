6194bb6338eb8f1bd3aa0b0d485f398076253f39
michael <mstclair@millionconcepts.com>
2024-03-29 15:56:06 -0400

    fix doesn't-do-the-thing-at-all bug in

--- pdr/parselabel/pds4.py
+++ pdr/parselabel/pds4.py
@@ -1,30 +1,43 @@
 """
-simple utilities for preprocessing pds4_tools-produced label objects for the
+Simple utilities for preprocessing pds4_tools-produced label objects for the
 pdr.Metadata constructor.
 """
 from collections import OrderedDict
+from typing import Hashable, MutableSequence, Any
 
 from dustgoggles.func import constant
 from dustgoggles.structures import dig_and_edit
 from multidict import MultiDict
 
 
-def log_and_pass(sequence, key, value):
-    """"""
+def log_and_pass(sequence: MutableSequence, key: Hashable, value: Any) -> Any:
+    """
+    Setter function for `dig_and_edit` that causes it to simply populate a
+    list of all keys at all levels of the MultiDict (so in effect not really a
+    'setter' function).
+    """
     sequence.append(key)
     return value
 
 
 def unpack_to_multidict(packed, mtypes=(dict,)):
-    """"""
+    """
+    Produce a MultiDict from any Mapping, with every element of `packed` that
+    is a list/tuple of `mtypes` converted into a seperate key of the
+    `MultiDict`, and other values unchanged. For instance:
+    ```
+    >>> unpack_to_multidict({'a': 1, 'b': [{'c': 2}, {'d': 3}]})
+    MultiDict({'a': 1, 'b': {'c': 2}, 'b': {'d': 3})
+    ```
+    """
     unpacked = MultiDict()
     for k, v in packed.items():
         if isinstance(v, (list, tuple)):
             if all([isinstance(thing, mtypes) for thing in v]):
                 for thing in v:
-                    unpacked[k] = thing
+                    unpacked.add(k, thing)
         else:
-            unpacked[k] = v
+            unpacked.add(k, v)
     return unpacked
 
 
