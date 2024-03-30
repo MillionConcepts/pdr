7b166b9ca1c281007ee54ea7b5bca16c5d35376d
michael <mstclair@millionconcepts.com>
2024-03-30 04:16:39 -0400

    change odyssey special case for pd 2.2.x compatibility

--- pdr/formats/odyssey.py
+++ pdr/formats/odyssey.py
@@ -1,10 +1,12 @@
 def map_table_loader(filename, fmtdef_dt):
-    """A few products open fine from their labels, but most do not. Seems like
-    a byte counting issue in the labels."""
+    """
+    A few products open fine from their labels, but most do not. Seems like
+    a byte counting issue in the labels.
+    """
     import pandas as pd
     names = [c for c in fmtdef_dt[0]['NAME'] if 'PLACEHOLDER' not in c]
-    # Some tables use tabs as column deliminators, others use spaces.
-    table = pd.read_csv(filename, header=None, delim_whitespace=True)
-    assert len(table.columns) == len(names)
+    # Some tables use tabs as column delimiters, others use spaces.
+    table = pd.read_csv(filename, header=None, sep=r"\s+")
+    assert len(table.columns) == len(names), "Mismatched column count"
     table.columns = names
     return table
