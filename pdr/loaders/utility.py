5b0d8e16a50222626647365d6f726c4692407934
michael <mstclair@millionconcepts.com>
2024-03-29 22:45:53 -0400

    add docstrings/hints to utility and cut an unused function

--- pdr/loaders/utility.py
+++ pdr/loaders/utility.py
@@ -1,6 +1,7 @@
 from functools import partial
 from operator import contains
 from pathlib import Path
+from typing import Collection
 import warnings
 
 from multidict import MultiDict
@@ -20,28 +21,25 @@ def trivial(*_, **__):
     """
     This is a trivial loader. It does not load. The purpose is to use
     for any pointers we don't want to load and instead simply want ignored.
+    It has been superseded by loaders.datawrap.Trivial, and is retained for
+    merely sentimental reasons.
     """
     pass
 
 
-def tbd(name: str, block: MultiDict, *_, **__):
-    """
-    This is a placeholder function for pointers that are
-    not explicitly supported elsewhere. It throws a warning and
-    passes just the value of the pointer.
-    """
-    warnings.warn(f"The {name} pointer is not yet fully supported.")
-    return block
-
-
-def looks_like_this_kind_of_file(filename: str, kind_extensions) -> bool:
-    """"""
+def looks_like_this_kind_of_file(
+    filename: str, kind_extensions: Collection[str]
+) -> bool:
+    """Does this file have any of these extensions?"""
     is_this_kind_of_extension = partial(contains, kind_extensions)
     return any(map(is_this_kind_of_extension, Path(filename.lower()).suffixes))
 
 
-def is_trivial(pointer) -> bool:
-    """"""
+def is_trivial(pointer: str) -> bool:
+    """
+    Returns True if this is the name of a data object we want to handle
+    trivally, in the sense that we never ever want to load it directly.
+    """
     # TIFF tags / headers should always be parsed by the TIFF parser itself
     if (
         ("TIFF" in pointer)
