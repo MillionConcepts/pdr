eacdb3dc3baea50729ffee3903b75dacaf0900cc
m-stclair <mstclair@millionconcepts.com>
2024-10-13 21:42:28 -0400

    replace all that complicated parsing with an explicit dict

--- pdr/pds4_datatypes.py
+++ pdr/pds4_datatypes.py
@@ -1,39 +1,45 @@
-import re
-
-PDS4_NUMERICAL_DTYPE_PAT = re.compile(
-    rf'(?P<cat>(Un)?[Ss]igned|Complex|IEEE754)'
-    rf'(?P<order>MSB|LSB)?'
-    rf'(?P<size>\d{1, 2}|Byte|BitString|Single|Double)'
-)
-
-IDICT = {'Unsigned': 'u', 'Signed': 'i'}
-ODICT = {'LSB': '<', 'MSB': '>'}
+# TODO: unclear how to interpret bit string types in the context of
+#  Element_Array -- there is no width specification! Is this ever
+#  actually used? Need examples.
+PDS4_DTYPE_MAPPING = {
+    "ComplexLSB16": "<c16",
+    "ComplexLSB8": "<c8",
+    "ComplexMSB16": ">c16",
+    "ComplexMSB8": ">c8",
+    "IEEE754LSBDouble": "<f8",
+    "IEEE754LSBSingle": "<f4",
+    "IEEE754MSBDouble": ">f8",
+    "IEEE754MSBSingle": ">f4",
+    "SignedBitString": None,
+    "SignedByte": "i1",
+    "SignedLSB2": "<i2",
+    "SignedLSB4": "<i4",
+    "SignedLSB8": "<i8",
+    "SignedMSB2": ">i2",
+    "SignedMSB4": ">i4",
+    "SignedMSB8": ">i8",
+    "UnsignedBitString": None,
+    "UnsignedByte": "u1",
+    "UnsignedLSB2": "<u2",
+    "UnsignedLSB4": "<u4",
+    "UnsignedLSB8": "<u8",
+    "UnsignedMSB2": ">u2",
+    "UnsignedMSB4": ">u4",
+    "UnsignedMSB8": ">u8"
+}
 
 
 def sample_types(sample_type: str) -> str:
-    """
-    Defines a translation from PDS3 physical data types to Python struct or
-    numpy dtype format strings, using both the type and byte width specified
-    (because the mapping to type alone is not consistent across PDS3).
-    """
-    parse = PDS4_NUMERICAL_DTYPE_PAT.match(sample_type)
-    if parse is None:
+    code = PDS4_DTYPE_MAPPING.get(sample_type)
+    if code is None:
         raise NotImplementedError(f"{sample_type} is not yet supported.")
-    if parse['cat'] == "IEEE":
-        ftype = {'Double': 'd', 'Single': 'f'}[parse['size']]
-        return f"{ODICT[parse['order']]}{ftype}"
-    if (itype := IDICT.get(parse['cat'])) is not None:
-        if parse['size'] == 'Byte':
-            return f"{itype}1"
-        if parse['size'] == 'BitString':
-            # TODO: unclear how to interpret this in the context of an
-            #  Element_Array -- there is no width specification! Is this ever
-            #  actually used? Need examples.
-            raise NotImplementedError(
-                'PDS4 integers in bit string representation are not currently '
-                'supported'
-            )
-        return f"{ODICT[parse['order']]}{itype}{parse['size']}"
-    if parse['cat'] == 'Complex':
-        return f"{ODICT[parse['order']]}c{parse['size']}"
-    raise NotImplementedError(f"{sample_type} is not yet supported.")
+    return code
+
+# PDS4_NUMERICAL_DTYPE_PAT = re.compile(
+#     rf'(?P<cat>(Un)?[Ss]igned|Complex|IEEE754)'
+#     rf'(?P<order>MSB|LSB)?'
+#     rf'(?P<size>\d{1, 2}|Byte|BitString|Single|Double)'
+# )
+#
+# IDICT = {'Unsigned': 'u', 'Signed': 'i'}
+# ODICT = {'LSB': '<', 'MSB': '>'}
